06a9b3ee1ade1786db4e5b5845d53461aaab7b59
Roushan Kumar <rk04393@gmail.com>
2025-06-14 23:30:23 +0530

    installed material ui and added one code in <Container>

--- email-assistant-frontend/src/App.jsx
+++ email-assistant-frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css'
+import {
+    Container,
+    Box,
+    Typography
+} from "@mui/material";
 
 function App() {
     return (
-        <>
-        </>
+        <Container>
+            <Box className="glass-card">
+                <Typography variant="h3" align="center" gutterBottom sx={{
+                    color: '#fff', fontWeight: 700
+                }}>
+                    Email Reply Assistant 🤖
+                </Typography>
+            </Box>
+        </Container>
     )
 }
 
