4df4effc499745e6d14d954d6518bd11cd274be4
Roushan Kumar <rk04393@gmail.com>
2025-06-14 22:53:17 +0530

    Service initial

--- email-assitant-backend/src/main/java/com/ai/emailassitantbackend/service/EmailGeneratorService.java
+++ email-assitant-backend/src/main/java/com/ai/emailassitantbackend/service/EmailGeneratorService.java
@@ -1,4 +1,25 @@
 package com.ai.emailassitantbackend.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
 public class EmailGeneratorService {
+
+    private final RestTemplate restTemplate; // Blocking HTTP client
+    private final ObjectMapper objectMapper; // ObjectMapper for JSON processing
+
+
+    @Value("${gemini.api.key}")
+    private String geminiAPIKey;
+    @Value("${gemini.api.url}")
+    private String geminiAPIURL;
+    @Value("${gemini.model.path}")
+    private String geminiModelPath;
+
+
+    public EmailGeneratorService(RestTemplate restTemplate, ObjectMapper objectMapper) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+    }
 }
