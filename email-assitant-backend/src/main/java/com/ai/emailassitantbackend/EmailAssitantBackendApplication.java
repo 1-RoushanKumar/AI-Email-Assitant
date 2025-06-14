40538c3eeed194578ce3e96272b8b55b9e4b302a
Roushan Kumar <rk04393@gmail.com>
2025-06-14 22:41:49 +0530

    Initial Backend file.

---
+++ email-assitant-backend/src/main/java/com/ai/emailassitantbackend/EmailAssitantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ai.emailassitantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailAssitantBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailAssitantBackendApplication.class, args);
+    }
+
+}
---
+++ email-assitant-backend/src/test/java/com/ai/emailassitantbackend/EmailAssitantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.emailassitantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAssitantBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
